1d72975e139e6e69a8961c905fcc55de58899234
Davi Neves <dneves02@gmail.com>
2020-09-28 16:01:54 -0700

    ok

--- web/Week02/head.php
+++ web/Week02/head.php
@@ -3,7 +3,7 @@
     <head>
         <title>?php echo($title); ?></title>
         <meta charset="UTF-8" />
-        <link rel="stylesheet" href="./Week02/mystyle.css">
+        <link rel="stylesheet" href="mystyle.css">
 
         <!-- CSS only -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
