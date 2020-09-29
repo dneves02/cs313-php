ce9d41039fde0ee8c6a9d57ee73242fe29acd0f1
Davi Neves <dneves02@gmail.com>
2020-09-28 22:41:16 -0700

    page 2

--- web/Week02/footer.php
+++ web/Week02/footer.php
@@ -3,7 +3,7 @@
                 <div id="div3" class="row div-later hover-bold">
                     <div class="col-md-12 col-sm-12 main" id="section4">
                         <footer>
-                            <a href="#div1">Back Up!</a><br>
+                            <a href="../index.php/#div1">Back!</a><br>
                             Author: Davi Neves<br>
                             <a href="mailto:dneves02@gmail.com">dneves02@gmail.com</a>
                         </footer>
