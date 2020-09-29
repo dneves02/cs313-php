0ec79aa6e07e1522b1414b4d5d670867bc5766a2
Davi Neves <dneves02@gmail.com>
2020-09-28 22:56:27 -0700

    page 2

--- web/Week02/header.php
+++ web/Week02/header.php
@@ -3,7 +3,6 @@
             <div class="row section">
 		        <div class="col-md-12 col-sm-12" id="div1">
 					<div class="banner-top center-text fill">
-						<!-- EXAMPLE OF INLINE STYLE -->
 						<span class="home-header hover-bold">Welcome to Davi Neves Home Page</span>
 						<br /> <a href="#section4">Send me an email</a>
 					</div>
