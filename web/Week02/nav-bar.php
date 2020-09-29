c9f123a0e1f09e1e70ced1f6cf28dcbb4c1ff666
Davi Neves <dneves02@gmail.com>
2020-09-28 22:30:04 -0700

    page 2

--- web/Week02/nav-bar.php
+++ web/Week02/nav-bar.php
@@ -5,8 +5,8 @@
 				<div class="col-md-12 col-sm-12 content-top fill" id="section0.4">
 					<div class="header">Index</div>
 					<ul class="b">
-						<li><a href="index.html">About Me</a></li>
-						<li><a href="index.html">Pictures</a></li>
+						<li><a href="./Week02/index.html">About Me</a></li>
+						<li><a href="./Week02/index.html">Pictures</a></li>
 						<li><a href="#section3">My Assignments</a></li>
 
 					</ul>
