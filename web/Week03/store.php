137679ffdbe75f6a8a63412c9ff473dbedd7984a
Davi Neves <dneves02@gmail.com>
2020-10-02 22:01:41 -0700

    updated

---
+++ web/Week03/store.php
@@ -0,0 +1,12 @@
+
+<?php $title = 'Bookstore';?>
+<?php $currentPage = 'Home';?>
+
+<?php include('./Week02/head.php');?>
+
+<body>
+    <div class="container">
+
+        <?php include('./Week02/header.php');?>
+        
+        <?php include('./Week02/footer.php');?>
