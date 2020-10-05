9c948ad82429fbffca45717e48ead0f76b603f43
Davi Neves <dneves02@gmail.com>
2020-10-05 13:44:13 -0700

    week03

---
+++ web/Week03/head.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title> <?php echo($title);?> </title>
+        <meta charset="UTF-8" />
+        <meta name="author" content="Davi Neves">
+
+        <!-- CSS only -->
+        <link rel="stylesheet" href="mystyle.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+        <!-- JS, Popper.js, and jQuery -->
+        <script src="./assign11.js"></script>
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+    </head>
+<head>
+    <!-- Search Engine Optimization -->
+	<title>Neves Bookstore</title>
+	<meta name="description" content="Neves Bookstore">
+	<meta name="keywords" content="Online, Bookstore, Books, Neves">
+	<meta name="author" content="Davi Neves">
+
+	<!-- responsive -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- encoding -->
+    <meta charset="UTF-8" />
+
+    <!-- styles -->
+	<link rel="stylesheet" href="./assign03.css" />
+
+    <!-- script -->
+    <script src="./assign03.js"></script>
+</head>
