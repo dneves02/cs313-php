77e5c428c6bcc7022624f180a2aa1cb203d1ea57
Davi Neves <dneves02@gmail.com>
2020-10-05 18:55:40 -0700

    update

--- web/Week03/assign03.php
+++ web/Week03/assign03.php
@@ -1,3 +1,5 @@
+<?php $title = 'Neves Bookstore';?>
+<?php $currentPage = 'Checkout';?>
 <?php
     $firstName =      $_GET ["first_name"];
     $lastName =       $_GET ["last_name"];
@@ -23,42 +25,16 @@
     $item_10 = $_GET ["item_10"];
     $item_11 = $_GET ["item_11"];
 ?>
+<?php include('head.php');?>
 
-<!DOCTYPE html>
-<html lang="en-US">
+<?php include('header.php');?>
 
-<head>
-    <!-- Search Engine Optimization -->
-	<title>Neves Bookstore - Purchase Review</title>
-	<meta name="description" content="Neves Bookstore">
-	<meta name="keywords" content="Online, Bookstore, Books, Neves">
-	<meta name="author" content="Davi Neves">
+<div class="container">
+        <div class="row">
+            <div class="col-9 col-s-9">
+                <section>
 
-	<!-- responsive -->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- encoding -->
-    <meta charset="UTF-8" />
-
-    <!-- styles -->
-	<link rel="stylesheet" href="./assign11.css" />	
-</head>
-
-<body>
-    <header>
-        <h2>Neves Bookstore</h2>
-    </header>
-
-    <div class="clearfix">
-        <section>
-			<h1>Purchase Review</h1>
-			<div class="row">
-				<div class="col-3 col-s-3 menu">
-					<div class="button-link-container">
-						<a href="#"><div class="button-link link-current">Children</div></a><br />
-						<a href="../index.html"><div class="button-link ">Index</div></a><br />
-					</div>
-				</div>
 
                 <div class="col-9 col-s-9">
                     <h2 class="alert-heading">Order Confirmation for
@@ -259,7 +235,8 @@
 
                     ?>
 
-                    <form action="assign11_a.php" method="GET" class="buttons">
+
+                    <form action="checkout.php" method="GET" class="buttons">
                         <h3>Do you want to submit this order?</h3>
                         <input id="confirm" type="submit" name="btn-submit" value="Confirm">
                         <input id="cancel" type="submit" name="btn-cancel" value="Cancel">                       
@@ -269,14 +246,5 @@
         </section>    
     </div>
 
-    <footer>
-		<br /><br />
-		Address. xxx XXX Portland, OR xxxxx<br />
-		Tel. 123-456-7890<br />      
-		Email. dneves@gmail.com<br /><br />
-		© 2020 by Neves.
-		<br /><br />
-		<a href="#">Contact Us!</a>
-    </footer>
-</body>
-</html>
+    <?php include('footer.php');?>
+    
