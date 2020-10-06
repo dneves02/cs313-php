820aefc92ad2b6b85847e10645bc22eea3e437e4
Davi Neves <dneves02@gmail.com>
2020-10-05 21:04:58 -0700

    assign03

--- web/Week03/header.php
+++ web/Week03/header.php
@@ -6,16 +6,16 @@
 	   </header>
 
       <div class="clearfix">
-        
+        <section>
 			<h1>Children Classic Books</h1>
 			   <div class="row">
 				   <div class="col-12 col-s-12 menu">
 					   <div class="button-link-container">
-						   <a href="store.php"><div class="button-link link-current">Children</div></a><br />
+						   <a href="store.php"><div class="button-link link-current">Bookstore</div></a><br />
 						   <a href="../index.php"><div class="button-link ">Index</div></a><br />
 					   </div>
                </div>
             </div>
-         
+         </section>
       </div>
    </div>   
