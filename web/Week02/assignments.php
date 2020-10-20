825dab5231918dc474c42badd4a565bc908fac2e
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-10-20 16:38:44 -0700

    ok

--- web/Week02/assignments.php
+++ web/Week02/assignments.php
@@ -3,13 +3,19 @@
             <div class="row section" id="section3">
 		        <div class="col-md-12 header">My Assignments
 		        	<ul class="centered fill">
-		        		<li><a href="./Week01/hello.html"><div class="button">Week 01<br /></div></a></li>
+		        			<li><a href="./Week01/hello.html"><div class="button">Week 01<br /></div></a></li>
 		                <li><a href="./Week02/intro.php"><div class="button">Week 02<br /></div></a></li>
 		                <li><a href="./Week03/store.php"><div class="button">Week 03<br /></div></a></li>
 		                <li><a href="./Week04/dbnote.txt"><div class="button">Week 04<br /></div></a></li>
 		                <li><a href="./Week05/base.php"><div class="button">Week 05<br /></div></a></li>
 		                <li><a href="./Week06/ta06.php"><div class="button">Week 06<br /></div></a></li>
-		                
+		                <li><a href="./week07/assign07.html"><div class="button">Week 07<br /></div></a></li>
+		                <li><a href="./week08/assign08.html"><div class="button">Week 08<br /></div></a></li>
+		                <li><a href="./week09/assign09.html"><div class="button">Week 09<br /></div></a></li>
+		                <li><a href="./week10/assign10.html"><div class="button">Week 10<br /></div></a></li>
+		                <li><a href="./week11/assign11.html"><div class="button">Week 11<br /></div></a></li>
+		                <li><a href="./week12/assign12.html"><div class="button">Week 12<br /></div></a></li>
+		                <li><a href="./week13/assign13.html"><div class="button">Week 13</div></a></li>
 		            </ul>
 		        </div>
 	        </div>
