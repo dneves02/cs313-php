a0ff87fc48869116d85d18d51f7f744ed26807e6
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-10-20 17:29:10 -0700

    Updates

--- web/Week02/assignments.php
+++ web/Week02/assignments.php
@@ -9,13 +9,13 @@
 		                <li><a href="./Week04/dbnote.txt"><div class="button">Week 04<br /></div></a></li>
 		                <li><a href="./Week05/base.php"><div class="button">Week 05<br /></div></a></li>
 		                <li><a href="./Week06/viewScripture.php"><div class="button">Week 06<br /></div></a></li>
-		                <li><a href="./week07/assign07.html"><div class="button">Week 07<br /></div></a></li>
-		                <li><a href="./week08/assign08.html"><div class="button">Week 08<br /></div></a></li>
-		                <li><a href="./week09/assign09.html"><div class="button">Week 09<br /></div></a></li>
-		                <li><a href="./week10/assign10.html"><div class="button">Week 10<br /></div></a></li>
-		                <li><a href="./week11/assign11.html"><div class="button">Week 11<br /></div></a></li>
-		                <li><a href="./week12/assign12.html"><div class="button">Week 12<br /></div></a></li>
-		                <li><a href="./week13/assign13.html"><div class="button">Week 13</div></a></li>
+		                <li><a href="./Week02/intro.php"><div class="button">Week 07<br /></div></a></li>
+		                <li><a href="./Week02/intro.php"><div class="button">Week 08<br /></div></a></li>
+		                <li><a href="./Week02/intro.php"><div class="button">Week 09<br /></div></a></li>
+		                <li><a href="./Week02/intro.php"><div class="button">Week 10<br /></div></a></li>
+		                <li><a href="./Week02/intro.php"><div class="button">Week 11<br /></div></a></li>
+		                <li><a href="./Week02/intro.php"><div class="button">Week 12<br /></div></a></li>
+		                <li><a href="./Week02/intro.php"><div class="button">Week 13</div></a></li>
 		            </ul>
 		        </div>
 	        </div>
--- web/Week05/dbConnect.php
+++ web/Week05/dbConnect.php
@@ -40,3 +40,4 @@ function get_db() {
 
 	return $db;
 }
+?>
