ed99e16a08c2eeb7205913ca0b5de3fe2a6de6da
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-10-24 20:40:36 -0700

    ok

--- web/team.php
+++ web/team.php
@@ -13,8 +13,8 @@
 				<div class="col-md-12 col-sm-12 content-top fill" id="section0.4">
 					<div class="header">Index</div>
 					<ul class="banner">
-						<li><a href="./Week05/base.php">MyTimeCard App</a></li>
-						<li><a href="./Week05/viewScripture.php">Team Activity</a></li>
+						<li><a href="./Week06/base.php">MyTimeCard App</a></li>
+						<li><a href="./Week06/TeamActivity/viewScripture.php">Team Activity</a></li>
 						<li><a href="index.php">My Assignments</a></li>
 					</ul>
 				</div>
