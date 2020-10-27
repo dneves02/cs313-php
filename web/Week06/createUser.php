576262762bf12fd1a7b97c6d3e6a50f5f7e3e465
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-10-26 23:23:14 -0700

    new user

--- web/Week06/createUser.php
+++ web/Week06/createUser.php
@@ -8,51 +8,63 @@
 
 require "../dbConnect.php";
 $db = get_db();
-$title = "MyTimeCard"
+$title = "Create User"
 ?>
 
 <?php include('header.php');?>
 <div class="container">
 	<div class="row section">
 		<div class="col-md-12 col-sm-12 content-top fill" id="create_user">
-		<h1>Create new user</h1>
+		<h1>Create New User</h1>
+		<form method="post" action="#">
+		First Name:<input type="text" name="first_name" size="20"><br>
+		Last Name:<input type="text" name="last_name" size="20"><br>
+		Email: <input type="email" name="email" size="20"><br>
+		DOB:<input type="date" name="birth_date" size="20"><br>
+		Hired Date:<input type="date" name="hire_date" size="20"><br>
+		Username:<input type="text" name="username" size="20"><br>
+		Password:<input type="text" name="password" size="20"><br>
+		<br>
+		<b></b>
+		<br>
+		
+		<button><a href="#" id="add">Create User</a></button>
+
+		</form>
+		
 		<?php
 
-
       // First, prepare the statement
+		$first_name = $_POST['first_name'];
+		$last_name = $_POST['last_name'];
+		$birth_date = $_POST['birth_date'];
+		$hire_date = $_POST['hire_date'];
+		$email = $_POST['email'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
 
-      $statement1 = $db->prepare("SELECT first_name, last_name, hire_date FROM employees");
-      $statement1->execute();
-      // Go through each result
+		$new_user = $db->prepare("INSERT INTO employees (first_name, last_name, birth_date, hire_date)
+								VALUES ($first_name, $last_name, $birth_date, $hire_date)");
+		$new_employee = $db->prepare("INSERT INTO public.user (username, password, email)
+								VALUES ($username, $password, $email )");
+      $new_employee->execute();
+		$new_user->execute();
+		
+		// Go through and check
+		$statement = $db->prepare("SELECT employees (first_name, last_name, birth_date, hire_date) WHERE (first_name = $first_name)");
+      $statement->execute();
       while ($row = $statement1->fetch(PDO::FETCH_ASSOC))
       {
          // The variable "row" now holds the complete record for that
          // row, and we can access the different values based on their
          // name
-         $first_name = $row['first_name'];
-         $last_name = $row['last_name'];
-         $birth_date = $row['birth_date'];
-         $hire_date = $row['hire_date'];
+         $first_name1 = $row['first_name'];
+         $last_name1 = $row['last_name'];
+         $birth_date1 = $row['birth_date'];
+         $hire_date1 = $row['hire_date'];
 
-         echo "<p><strong>Employee: $first_name $last_name, DOB: $birth_date, hired: $hire_date.</strong></p>";
+         echo "<p><strong>Employee: $first_name1 $last_name1, DOB: $birth_date1, hired: $hire_date1.</strong></p>";
       }
-
-      $statement2 = $db->prepare("SELECT shift_date, shift_type, clock_in, clock_out FROM time_card");
-      $statement2->execute();
-      // Go through each result
-      while ($row = $statement2->fetch(PDO::FETCH_ASSOC))
-      {
-         // The variable "row" now holds the complete record for that
-         // row, and we can access the different values based on their
-         // name
-         $shift_date = $row['shift_date'];
-         $shift_type = $row['shift_type'];
-         $clock_in = $row['clock_in'];
-         $clock_out = $row['clock_out'];
-
-         echo "<p>Date: $shift_date - Shift: $shift_type , Start: $clock_in - End: $clock_out</p>";
-      }
-
       ?>
       </div>
    </div>
