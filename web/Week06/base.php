28be621574975b3ec68bbc98817b5a87ec8ef511
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-10-26 19:58:29 -0700

    OK

--- web/Week06/base.php
+++ web/Week06/base.php
@@ -30,29 +30,6 @@ $title = "MyTimeCard"
 		</div>
 	</div>
 
-<?php
-
-
-// First, prepare the statement
-
-$statement1 = $db->prepare("SELECT first_name, last_name, hire_date FROM employees");
-$statement1->execute();
-// Go through each result
-while ($row = $statement1->fetch(PDO::FETCH_ASSOC))
-{
-	// The variable "row" now holds the complete record for that
-	// row, and we can access the different values based on their
-	// name
-	$first_name = $row['first_name'];
-	$last_name = $row['last_name'];
-	$birth_date = $row['birth_date'];
-	$hire_date = $row['hire_date'];
-
-	echo "<p><strong>Employee: $first_name $last_name, DOB: $birth_date, hired: $hire_date.</strong></p>";
-}
-
-?>
-
 
 </div>
 
--- web/Week06/header.php
+++ web/Week06/header.php
@@ -18,13 +18,14 @@
     </head>
     <body>
     <div class="container">
-      <div class="row section">
-         <div class="col-md-12 col-sm-12" id="div1">
-            <div class="banner-top center-text fill">
-               <span class="home-header hover-bold">Welcome to MyTimeCard App</span>
-               <br /> <a href="#section4">Contact Us</a>
+        <div class="row section">
+            <div class="col-md-12 col-sm-12" id="div1">
+                <div class="banner-top center-text fill">
+                    <span class="home-header hover-bold">Welcome to MyTimeCard App</span>
+                    <a href="#section4">Contact Us</a>
+                </div>
+                <div class="header center-text">Brigham Young University &ndash; Idaho</div>
+                <div class="section-header center-text">CS 341 Web Backend Development</div>
             </div>
-            <div class="header center-text">Brigham Young University &ndash; Idaho</div>
-         <div class="section-header center-text">CS 341 Web Backend Development</div>
-      </div>
+        </div>
     </div>
