6b5dd257290b9ab9f0ddecd3e48d6a3208e35db5
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-10-26 20:20:30 -0700

    ok

--- web/Week06/base.php
+++ web/Week06/base.php
@@ -20,7 +20,7 @@ $title = "MyTimeCard"
 		<div class="col-md-12 col-sm-12 content-top fill" id="section0.4">
 			<div class="header">Index</div>
 				<ul class="banner">
-					<li><a href="login.php">Login to MyTimeCard App</a></li>
+					<li><a href="login.php">Login</a></li>
 					<li><a href="createUser.php">Create New User</a></li>
 					<li><a href="clockin.php">Clock in/Clock out</a></li>
 					<li><a href="myHours.php">My Hours</a></li>
