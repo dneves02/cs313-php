19b5d73333e5536b66a180da5ea3ca9c9460ec9c
dneves02 <dneves02@gmail.com>
2020-11-02 11:15:53 -0800

    update

--- web/Week07/login.php
+++ web/Week07/login.php
@@ -16,6 +16,22 @@ $title = "MyTimeCard"
 	<div class="row section">
 		<div class="col-md-12 col-sm-12 content-top fill" id="login">
       <h1>Login</h1>
+
+      <form id="create_user" method="POST" action="insertUser.php">
+						
+				<input type="text" name="username" size="16">
+				<label for="username">Username: </label> <br>
+				
+				<input type="text" name="user_password" size="16">
+				<label for="user_password">Password: </label> <br>
+
+
+
+			<br>
+			
+			<button><input type="submit" id="add" value="Create User"></button>
+
+			</form>
       <?php
 
 
