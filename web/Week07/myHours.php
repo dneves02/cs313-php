152b64d07aa7957e9190e444dabb554ef2ae4f0c
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-11-02 17:51:59 -0800

    my hours

--- web/Week07/myHours.php
+++ web/Week07/myHours.php
@@ -15,46 +15,22 @@ $title = "MyTimeCard"
 <div class="container">
 	<div class="row section">
 		<div class="col-md-12 col-sm-12 content-top fill" id="my_hours">
-      <h1>Payroll Hours</h1>
+      <h1>My Payroll Hours</h1>
 
-      <?php
+      <form id="get_hours" method="POST" action="getMyHours.php">
+				
+				<input type="text" name="username" size="16">
+				<label for="username">Username: </label> <br>
+				
+				<input type="text" name="user_password" size="16">
+				<label for="user_password">Password: </label> <br>
 
+			<br>
+			
+			<button><input type="submit" id="time_card" value="My Hours"></button>
 
-      // First, prepare the statement
+			</form>
 
-      $statement1 = $db->prepare("SELECT first_name, last_name, hire_date FROM employees");
-      $statement1->execute();
-      // Go through each result
-      while ($row = $statement1->fetch(PDO::FETCH_ASSOC))
-      {
-         // The variable "row" now holds the complete record for that
-         // row, and we can access the different values based on their
-         // name
-         $first_name = $row['first_name'];
-         $last_name = $row['last_name'];
-         $birth_date = $row['birth_date'];
-         $hire_date = $row['hire_date'];
-
-         echo "<p><strong>Employee: $first_name $last_name, DOB: $birth_date, hired: $hire_date.</strong></p>";
-      }
-
-      $statement2 = $db->prepare("SELECT shift_date, shift_type, clock_in, clock_out FROM time_card");
-      $statement2->execute();
-      // Go through each result
-      while ($row = $statement2->fetch(PDO::FETCH_ASSOC))
-      {
-         // The variable "row" now holds the complete record for that
-         // row, and we can access the different values based on their
-         // name
-         $shift_date = $row['shift_date'];
-         $shift_type = $row['shift_type'];
-         $clock_in = $row['clock_in'];
-         $clock_out = $row['clock_out'];
-
-         echo "<p>Date: $shift_date - Shift: $shift_type , Start: $clock_in - End: $clock_out</p>";
-      }
-
-      ?>
       </div>
    </div>
 </div>
