7bdf09fd39dbb5a00d8d98dd5c8e9c26ec2f9fa2
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-11-02 23:55:30 -0800

    clock out

--- db/db_inserts.sql
+++ db/db_inserts.sql
@@ -50,8 +50,8 @@ CREATE TABLE time_card (
     id SERIAL NOT NULL PRIMARY KEY,
     shift_date DATE NOT NULL,
     shift_type VARCHAR NOT NULL,
-    clock_time TIME,
-    clock_type VARCHAR,
+    clock_in TIME,
+    clock_out TIME,
 );  
 
 #Select * From time_card;
