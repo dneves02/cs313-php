302b704cb41b31aa93c8c2d06c580974a610aab3
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-11-03 00:00:18 -0800

    clock

--- web/Week07/clockin.php
+++ web/Week07/clockin.php
@@ -18,16 +18,12 @@ $title = "MyTimeCard"
 		<div class="col-md-12 col-sm-12 content-top fill" id="clockin">
       <h1>Clock in / Clock out</h1>
 
-         <form id="create_user" method="POST" action="insertTime.php">       
+         <form id="clock" method="POST" action="insertTime.php">       
                <button><input type="submit" id="clock_in" value="Clock In"></button>
                <br>
                <br>
                <button><input type="submit" id="clock_out" value="Clock Out"></button>      
          </form>
-
-
-
-      <?php
      
       </div>
    </div>
